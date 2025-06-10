d77cca5472f3a35735a122cb990c8a9bb99c4d5f
sayyed rabeeh <sayyedrabeeh240@gmail.com>
2025-06-10 20:39:41 +0530

    m change

--- dino.py
+++ dino.py
@@ -3,16 +3,16 @@ import mediapipe as mp
 import pyautogui
 import time
 import pygetwindow as gw
-import win32gui
+import win32gui  
 import win32con
 import numpy as np 
 
-cv2.namedWindow("Play Dino ")
+cv2.namedWindow("Hand Gesture Control")
 dummy_frame = 255 * np.ones((100, 100, 3), dtype=np.uint8)
-cv2.imshow("Play Dino", dummy_frame)
+cv2.imshow("Hand Gesture Control", dummy_frame)
 cv2.waitKey(1)
 try:
-    hwnd = gw.getWindowsWithTitle("Play Dino")[0]._hWnd
+    hwnd = gw.getWindowsWithTitle("Hand Gesture Control")[0]._hWnd
     win32gui.SetWindowPos(hwnd, win32con.HWND_TOPMOST, 100, 100, 640, 480, 0)
 except Exception as e:
     print("⚠️ Could not set window always on top:", e)
@@ -44,7 +44,7 @@ while cap.isOpened():
                 pyautogui.press('space') 
                 print("🕹️ Jump triggered!")
                 last_press_time = time.time()
-    cv2.imshow("Play Dino", img)
+    cv2.imshow("Hand Gesture Control", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
